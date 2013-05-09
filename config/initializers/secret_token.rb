afcce0d214b479e0416e7d3c9d48e650b515007e
Tyler Morgan <toasty@localhost.localdomain>
2013-05-08 19:28:15 -0700

    New RoR project for FanVoice

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FanVoice::Application.config.secret_token = 'f74dd66b9653e6ce7621abf6a3176e1c08820be71e3d9d4f636d45b18c566b981f6fa6edfa2748904b6d3d9323dbc5f7fdeff8090ee6645003b365d29984d780'
