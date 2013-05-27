39733187d3675ca3e7336a4fbd1c41d145a6a4f2
Andrew Shenstone <shenst1@gmail.com>
2013-05-27 11:18:29 -0700

    All rake specs passing. Basically ticketee all the way through chapter 6.

--- spec/integration/signing_in_spec.rb
+++ spec/integration/signing_in_spec.rb
@@ -15,7 +15,8 @@ feature 'Signing in' do
     User.find_by_email('test@fanvoice.com').confirm!
     visit '/'
     click_link 'Sign in'
-    fill_in 'Email', :with => "password"
+    fill_in 'Email', :with => "test@fanvoice.com"
+    fill_in 'Password', :with => "password"
     click_button "Sign in"
     page.should have_content("Signed in successfully.")
   end
