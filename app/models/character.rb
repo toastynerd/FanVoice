840fb2dea2dba88686920cc6cdecc3a9b874d93e
tylermorgan86 <tylermorgan86@gmail.com>
2013-05-28 09:50:55 -0700

    udpated readme

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,7 @@
 class Character < ActiveRecord::Base
   attr_accessible :bio, :name, :handle
+
+  validates :name, :presence => true
+  validates :bio, :presence => true
+  validates :handle, :presence => true
 end
