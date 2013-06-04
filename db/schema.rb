1d97b093df168f947d715e2df77da542be402675
Andrew Shenstone <shenst1@gmail.com>
2013-06-04 16:17:55 -0700

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130529172639) do
+ActiveRecord::Schema.define(:version => 20130530185724) do
 
   create_table "characters", :force => true do |t|
     t.string   "name"
