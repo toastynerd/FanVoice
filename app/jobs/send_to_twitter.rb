0a0149bc86759b93ed8293b44381712ed3508e27
tylermorgan86 <tylermorgan86@gmail.com>
2013-06-23 16:58:42 -0700

    added logging to SendToTwitter

--- app/jobs/send_to_twitter.rb
+++ app/jobs/send_to_twitter.rb
@@ -3,20 +3,20 @@ require 'twitter'
 class SendToTwitter
   @queue = :tweet_queue
   def self.perform(tweet_id, character_id)
-    puts "sending tweet"
     if @tweet = Tweet.find(tweet_id)
-      puts "found tweet"
       if @character = Character.find(character_id)
-        puts "found character"
         if @twitter_character = TwitterCharacter.find(@character.twitter_character_id)
-          puts "found twitter character"
           if @twitter_character.twitter.update(@tweet.body)
-            puts "Sent tweet for #{@tweet.post_at} at #{Time.now} with #{@twitter_character.name}"
+            Rails.logger.info "Sent tweet for #{@tweet.post_at} at #{Time.now} with #{@twitter_character.name}"
           end
+        else
+          Rails.logger.error "Could not find twitter character for #{@character.name}"  
         end
+      else
+        Rails.logger.error "Could not find character for id# #{character_id}"
       end
     else
-      puts "Could not send tweet."
+      Rails.logger.error "Could not find tweet # #{tweet_id}"
     end
   end
 end
