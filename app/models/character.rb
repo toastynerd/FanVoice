372f7de0d303b5bae6cb6b552c31bb97ac46c61d
tylermorgan86 <tylermorgan86@gmail.com>
2013-06-25 13:22:24 -0700

    fixed all specs except hiding links

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,9 +1,12 @@
 class Character < ActiveRecord::Base
-  attr_accessible :bio, :name, :handle, :image, :remote_image_url, :crop_x, :crop_y, :crop_w, :crop_h
+  attr_accessible :bio, :name, :handle, :image, :remote_image_url, :crop_x, :crop_y, :crop_w, :crop_h, :source_material_id
+
   #image uploading features
+
   mount_uploader :image, ImageUploader
   attr_accessor :crop_x, :crop_y, :crop_w, :crop_h
   after_update :crop_image
+
   def crop_image
     image.recreate_versions! if crop_x.present?
   end
--- spec/controllers/character_controller_spec.rb
+++ spec/controllers/character_controller_spec.rb
@@ -7,46 +7,54 @@ describe CharactersController do
   context "standard users" do
     it "cannot access a character for a source_material" do
       sign_in(:user, user)
-      get :show, :id => character.id, :source_material_id => source_material.id
+      get :show, :id => character.id
       response.should redirect_to(root_path)
       flash[:alert].should eql("The source_material you were looking for could not be found.")
     end
+
     context "with permission to view the source_material" do
       before do
         sign_in(:user, user)
         define_permission!(user, "view", source_material)
       end
+
       def cannot_create_characters!
-        response.should redirect_to(source_material)
+        response.should redirect_to(root_path)
         message = "You cannot create characters on this source_material."
         flash[:alert].should eql(message)
       end
+
       def cannot_update_characters!
-        response.should redirect_to(source_material)
+        response.should redirect_to(root_path)
         flash[:alert].should eql("You cannot edit characters on this source_material.")
       end
+
       it "cannot begin to create a character" do
-        get :new, :source_material_id => source_material.id
+        get :new, 
         cannot_create_characters!
       end
+
       it "cannot create a character without permission" do
-        post :create, :source_material_id => source_material.id
+        post :create, 
         cannot_create_characters!
       end
+
       it "cannot edit a character without permission" do
-        get :edit, { :source_material_id => source_material.id, :id => character.id }
+        get :edit, { :id => character.id }
         cannot_update_characters!
       end
+
       it "cannot update a character without permission" do
-        put :update, { :source_material_id => source_material.id,
+        put :update, { 
                        :id => character.id,
                        :character => {}
                      }
         cannot_update_characters!
       end
+
       it "cannot delete a character without permission" do
-        delete :destroy, { :source_material_id => source_material.id, :id => character.id }
-        response.should redirect_to(source_material)
+        delete :destroy, { :id => character.id }
+        response.should redirect_to(root_path)
         message = "You cannot delete characters from this source_material."
         flash[:alert].should eql(message)
       end
