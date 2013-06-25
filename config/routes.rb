3521e510001fe7dd3bf929c149295d2ce5c1868a
Andrew Shenstone <shenst1@gmail.com>
2013-06-25 10:03:59 -0700

    Get ready for merge to staging.

--- config/routes.rb
+++ config/routes.rb
@@ -30,7 +30,7 @@ FanVoice::Application.routes.draw do
 
   #resque background task monitor available for only admins needs to be added
 
-   mount Resque::Server, :at => "/resque"
+  mount Resque::Server, :at => "/resque"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
