11b1764373a6f0082c7c9b858b179eb09093d058
Andrew Shenstone <shenst1@gmail.com>
2013-07-22 16:30:07 -0700

    Configure carrier wave so that it uses file storage in dev and test, and fog (for s3) in production.

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -10,4 +10,20 @@ CarrierWave.configure do |config|
   config.fog_directory  = ENV["AWS_BUCKET"]                     # required
   #  config.fog_public     = false                                   # optional, defaults to true
   # config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
+  if Rails.env.test? or Rails.env.cucumber?
+    CarrierWave.configure do |config|
+      config.storage = :file
+      config.enable_processing = false
+    end
+  end
+  if Rails.env.development?
+    CarrierWave.configure do |config|
+      config.storage = :file
+    end
+  end
+  if Rails.env.production?
+    CarrierWave.configure do |config|
+      config.storage = :fog
+    end
+  end
 end
