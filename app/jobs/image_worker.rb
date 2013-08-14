5055dccabba0c2aaddf5923f765fee59626071c4
Andrew Shenstone <shenst1@gmail.com>
2013-08-14 11:43:44 -0700

    Add recommended initializer to establish connection to database.

--- app/jobs/image_worker.rb
+++ app/jobs/image_worker.rb
@@ -1,8 +1,13 @@
 class ImageWorker
   @queue = :image_queue
   def self.perform(id, key)
-    sleep 10
+    # begin
     character = Character.find(id)
+    #   raise 'An error has occurred'
+    # rescue
+    #   sleep 20
+    #   character = Character.find(id)
+    # end
     character.key = key
     character.remote_image_url = character.image.direct_fog_url(with_path: true)
     character.save!
